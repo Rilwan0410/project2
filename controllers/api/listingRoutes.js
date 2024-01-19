7a58077ab15a3687396b4a672915eec3b46e867c
unknown <dannysaravia100@gmail.com>
2024-01-18 19:41:00 -0500

    renamed projectRoutes to listingRoutes and changed the code in file

--- controllers/api/listingRoutes.js
+++ controllers/api/listingRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Listings } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newListing = await Listings.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newlisting);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,19 +17,19 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const ListingData = await Listing.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
 
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    if (!ListingData) {
+      res.status(404).json({ message: 'No listings found with this id!' });
       return;
     }
 
-    res.status(200).json(projectData);
+    res.status(200).json(ListingData);
   } catch (err) {
     res.status(500).json(err);
   }
