753eff4f2a3b54355429ea419169f42bd8dddb1b
User <user@Users-MacBook-Air.local>
2024-01-29 17:51:00 -0500

    trying to fix bug

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ if (process.env.JAWSDB_URL) {
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: process.env.MY_SQL_PORT || 3306
+      port: process.env.MY_SQL_PORT || process.env.PORT
     }
   );
 }
