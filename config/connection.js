7e0636a6474f786fa312a767df5d40298440fa4d
User <user@Users-MacBook-Air.local>
2024-01-29 17:39:50 -0500

    bug fixes

--- config/connection.js
+++ config/connection.js
@@ -6,16 +6,11 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: process.env.MY_SQL_PORT || 3306
-    }
-  );
+  sequelize = new Sequelize('sneakers_db', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 8889 || 3306,
+  });
 }
 
 module.exports = sequelize;
